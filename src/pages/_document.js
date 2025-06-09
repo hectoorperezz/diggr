37dffd0f4a13272b838f6c0195e565e91ab763ae
hectoorperezz <hectorperezled02@gmail.com>
2025-06-09 18:17:09 +0200

    ads.txt_added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,23 +8,11 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <link rel="apple-touch-icon" href="/images/diggr.png" />
         
-        {/* Google AdSense Script - Solo funcionará en producción */}
+        {/* Google AdSense Verification Script */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3838039470797804"
           crossOrigin="anonymous"
-        />
-        
-        {/* Google AdSense Auto Ads - Opcional */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: "ca-pub-3838039470797804",
-                enable_page_level_ads: true
-              });
-            `
-          }}
         />
       </Head>
       <body>
